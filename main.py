c33f77f57997f419124737b551b7e5cd76106a8c
DaneMarc <dane0515@gmail.com>
2024-04-06 15:08:18 +0800

    Update embedding class and main program

--- main.py
+++ main.py
@@ -1,6 +1,110 @@
-from nlp.models.mlp import run_mlp
-from nlp.models.lstm import run_lstm
+import pandas as pd
+from sklearn.metrics import f1_score, precision_score, recall_score, accuracy_score, roc_auc_score, average_precision_score
+
+import warnings
+import torch
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+from nlp.embedding.embed import Embedding
+from nlp.models.mlp import MLP
+from nlp.models.lstm import LSTM
+
+N_CLASSES = 4
+
+def main(nEpochs=5, lr=0.00005):
+    warnings.filterwarnings("ignore")
+    
+    # Initialise
+    model = MLP(nHiddenChannels1=800, nHiddenChannels2=400) # set number of hidden nodes in each layer here
+    criterion = torch.nn.CrossEntropyLoss()
+    optimizer = torch.optim.Adam(model.parameters(), lr=lr)
+    
+    embed = Embedding(type='fasttext')
+    train_data, test_data = pd.read_csv('nlp/models/way1_train.csv'), pd.read_csv('nlp/models/way1_test.csv')
+    freqCutOff = int(len(train_data['text_lowercase'])*0.8)
+        
+    # Get word vectors for each unique token
+    x, dim = embed.get_embedding(train_data['text_lowercase'])
+    x_train = x[:freqCutOff]
+    x_val = x[freqCutOff:]
+    x_test, dim = embed.get_embedding(test_data['text_lowercase'])
+
+    # no. cols * 300, no. cols * (100 * 400)
+    
+    # x_train, x_val, x_test = [[sum(x) for x in zip(*embed.get_embedding(i.split()))] for i in x_train], [[sum(x) for x in zip(*embed.get_embedding(i.split()))] for i in x_val], [[sum(x) for x in zip(*embed.get_embedding(i.split()))] for i in x_test]
+    y_train, y_val, y_test = [i-1 for i in train_data['Label'][:freqCutOff]], [i-1 for i in train_data['Label'][freqCutOff:]], [i-1 for i in test_data['Label']]
+    
+    # Helper functions for training, testing, and generating word vectors
+    def train(x_train, y_train):
+        model.train()
+        losses = []
+        for i in range(len(x_train)):
+            optimizer.zero_grad()
+            
+            x, y = torch.tensor(x_train[i], dtype=torch.float32), torch.tensor([y_train[i]], dtype=torch.int64)
+            out = model(x)
+            out = torch.reshape(out, (1,N_CLASSES))
+            loss = criterion(out, y)
+            loss.backward()
+            optimizer.step()
+            losses.append(loss.item())
+            
+        return losses
+    
+    def test(x_test):
+        model.eval()
+        res = []
+        for i in x_test:
+            out = torch.argmax(model(torch.tensor(i, dtype=torch.float32))).item()
+            res.append(out)
+
+        return res
+    
+    print("Starting training...")
+    
+    losses = []
+    f1s, precisions, recalls, accs, rocs, prcs = [], [], [], [], [], []
+    rocs, prcs = [], []
+    for i in range(nEpochs):
+        currLosses = train(x_train, y_train)
+        losses.extend(currLosses)
+        minLoss, avgLoss = min(losses), sum(losses)/len(losses)
+
+        y_pred = test(x_val)
+        f1, precision, recall, acc, roc, prc = f1_score(y_val, y_pred, average='macro'), precision_score(y_val, y_pred, average='macro'), recall_score(y_val, y_pred, average='macro'), accuracy_score(y_val, y_pred), roc_auc_score(y_val, y_pred, average='macro'), average_precision_score(y_val, y_pred, average='macro')
+        f1s.append(f1); precisions.append(precision); recalls.append(recall); accs.append(acc); rocs.append(roc); prcs.append(prc)
+        
+        print(f'''Val Scores.
+                Epoch: {i} | Min Train Loss: {minLoss} | Avg Train Loss: {avgLoss}
+                Accuracy: {acc:.4f} | Precision: {precision:.4f} | Recall: {recall:.4f} | F1: {f1:.4f} | ROC: {roc:.4f} | PRC: {prc:.4f}''')
+        
+    # Visualisation of loss.
+    losses_float = [float(loss) for loss in losses] 
+    loss_indices = [i for i,l in enumerate(losses_float)] 
+    d = {'indices': np.array(loss_indices), 'loss': np.array(losses_float)}
+    pdnumsqr = pd.DataFrame(d)
+    sns.lineplot(x='indices', y='value', hue='variable', data=pd.melt(pdnumsqr, ['indices']))
+    plt.ylim(0, 20)
+    plt.show()
+    
+    # Visualisation of performance
+    epoch = [i for i in range(nEpochs)] 
+    d = {'epoch': np.array(epoch), 'ROC': np.array(rocs), 'PRC': np.array(prcs), 'F1': np.array(f1s), 'Precision': np.array(precisions), 'Recall': np.array(recalls), 'Accuracy': np.array(accs)}
+    pdnumsqr = pd.DataFrame(d)
+    sns.lineplot(x='epoch', y='value', hue='variable', data=pd.melt(pdnumsqr, ['epoch']))
+    plt.show()
+        
+    # Generate prediction on test data
+    y_pred = test(x_test)
+    f1 = f1_score(y_test, y_pred, average='macro')
+    precision = precision_score(y_test, y_pred, average='macro')
+    recall = recall_score(y_test, y_pred, average='macro')
+    acc = accuracy_score(y_test, y_pred)
+    print(f'''Test Scores.
+            Accuracy: {acc:.4f} | Precision: {precision:.4f} | Recall: {recall:.4f} | F1: {f1:.4f} ''')
+
 
 if __name__ == "__main__":
-    # run_mlp()
-    run_lstm()
+    main()
--- nlp/embedding/embed.py
+++ nlp/embedding/embed.py
@@ -2,68 +2,111 @@ import os
 import numpy as np
 import gensim.downloader as api
 
-from gensim.models import Word2Vec, FastText, doc2vec
+from gensim.models import Word2Vec, FastText
+from sklearn.feature_extraction.text import TfidfVectorizer
 
 class Embedding:
-    def __init__(self, type='word2vec', train=False, max_seq_len=630, path=None, docs=[]):
+    def __init__(self, type='word2vec', max_seq_len=500, path=None, docs=[]):
         self.max_seq_len = max_seq_len
         self.type = type
 
-        if train:
-            if type == 'word2vec':
-                if path:
-                    model = Word2Vec.load(path)
-                    model.train(docs, total_examples=len(docs), epochs=model.epochs)
-                else:
-                    if docs:
-                        model = Word2Vec(sentences=docs, vector_size=300)
-            elif type == 'fasttext':
-                if path:
-                    model = FastText.load(path)
-                    model.train(docs, total_examples=len(docs), epochs=model.epochs)
-                else:
-                    if docs:
-                        model = FastText(sentences=docs, vector_size=300)
-            elif type == 'doc2vec':
-                if path:
-                    model = doc2vec.Doc2Vec.load(path)
-                    model.train(docs, total_examples=len(docs), epochs=model.epochs)
-                else:
-                    docs = [doc2vec.TaggedDocument(doc, [i]) for i, doc in enumerate(docs)]
-                    model = doc2vec.Doc2Vec(documents=docs, vector_size=300, dm=1)
-            else:
-                print('Invalid type')
-
-            model.save(fname=os.path.join('embedding', 'models', type + '.model'))
-            self.model = model
+        if type == 'word2vec':
+            self.model = api.load('word2vec-google-news-300') if not path else Word2Vec.load(path).wv
+        elif type == 'glove':
+            self.model = api.load('glove-wiki-gigaword-300')
+        elif type == 'fasttext':
+            self.model = api.load('fasttext-wiki-news-subwords-300') if not path else FastText.load(path).wv
         else:
-            if type == 'word2vec':
-                self.model = api.load('word2vec-google-news-300') if not path else Word2Vec.load(path).wv
-            elif type == 'glove':
-                self.model = api.load('glove-wiki-gigaword-300')
-            elif type == 'fasttext':
-                self.model = api.load('fasttext-wiki-news-subwords-300') if not path else FastText.load(path).wv
-            else:
-                print('Invalid type')
+            print('Invalid type')
 
     # input a list of tokens
-    def get_embedding(self, doc):
-        if self.type == 'doc2vec':
-            return self.model.infer_vector(doc)
-        else:
-            doc_embedding = []
-            for token in doc:
-                if token in self.model:
-                    doc_embedding.append(self.model[token])
+    def get_embedding(self, docs, doc_embed=True, tfidf=False, sentiment=False):
+        docs = []
+
+        if tfidf:
+            vectorizer = TfidfVectorizer(min_df=0, max_df=9999999)
+            tfidf_scores = vectorizer.fit_transform(docs)
+
+        if doc_embed:
+            for doc in docs:
+                doc_embedding = []
+
+                if tfidf:
+                    weights = []
+                    for i, token in enumerate(doc):
+                        if token in self.model and token in vectorizer.vocabulary_:
+                            doc_embedding.append(self.model[token])
+                            weights.append(tfidf_scores[i, vectorizer.vocabulary_[token]])
                 else:
-                    doc_embedding.append(np.zeros(300))
+                    for token in doc:
+                        if token in self.model:
+                            doc_embedding.append(self.model[token])
 
-            # pad or clip the sequence
-            if len(doc_embedding) < self.max_seq_len:
-                doc_embedding += [np.zeros(300)] * (self.max_seq_len - len(doc_embedding))
-            else:
-                # TODO: use PCA to reduce the dimension
-                doc_embedding = doc_embedding[:self.max_seq_len]
+                if len(doc_embedding) == 0:
+                    doc_embedding = np.zeros((300,))
+                else:
+                    if tfidf:
+                        doc_embedding = np.average(doc_embedding, axis=0, weights=weights)
+                    else:
+                        doc_embedding = np.mean(doc_embedding, axis=0)
 
-            return doc_embedding
+                docs.append(doc_embedding)
+        else:
+            for doc in docs:
+                doc_embedding = []
+                weights = []
+
+                if tfidf:
+                    for i, token in enumerate(doc):
+                        if token in self.model and token in vectorizer.vocabulary_:
+                            tfidf_score = tfidf_scores[i, vectorizer.vocabulary_[token]]
+                            doc_embedding.append(self.model[token] * tfidf_score)
+                            weights.append(tfidf_score)
+                else:
+                    for token in doc:
+                        if token in self.model:
+                            doc_embedding.append(self.model[token])
+
+                if len(doc_embedding) > 0:
+                    doc_embedding = self.pad(doc_embedding, weights)
+                else:
+                    doc_embedding = np.zeros((self.max_seq_len * 300))
+
+                docs.append(doc_embedding)
+        
+        length = 300 if doc_embed else 300 * self.max_seq_len
+
+        return docs, length
+    
+
+    def pad(self, doc, weights):
+        if len(doc) < self.max_seq_len:
+            doc = np.array(doc)
+            mean = np.mean(doc, axis=0)
+            zeros = np.repeat([mean], self.max_seq_len - len(doc), axis=0)
+            doc = np.vstack((doc, zeros))
+            return doc.flatten()
+        elif len(doc) > self.max_seq_len:
+            # clip
+            #return np.array(self.tfidf_clip(doc, weights)).flatten()
+            return np.array(doc[:self.max_seq_len]).flatten()
+
+            # pca
+            #doc = np.array(doc).T
+            #doc = self.pca.fit_transform(doc)
+            #return doc.T.flatten()
+        else:
+            return np.array(doc).flatten()
+        
+    def tfidf_clip(self, doc, weights):
+        new_doc = []
+        sorted = weights.copy()
+        sorted.sort(reverse=True)
+        min_weight = sorted[self.max_seq_len-1]
+        
+        for i in range(len(doc)):
+            if weights[i] >= min_weight and len(new_doc) < self.max_seq_len:
+                new_doc.append(doc[i])
+            
+        return new_doc
     
